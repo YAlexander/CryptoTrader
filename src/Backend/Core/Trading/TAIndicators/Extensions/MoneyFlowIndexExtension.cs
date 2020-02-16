458f376e9dd23c935ccc747c97c50bcc8cfe1fe0
Alexander Yermakov <alexandr.v.ermakov@gmail.com>
2020-02-16 09:02:21 +0200

    CT-V2 Added Mfi Indicator

---
+++ src/Backend/Core/Trading/TAIndicators/Extensions/MoneyFlowIndexExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Contracts;
+using core.Trading.TAIndicators.Options;
+using core.Trading.TAIndicators.Results;
+
+namespace Core.Trading.TAIndicators.Extensions
+{
+    public static class MoneyFlowIndexExtension
+    {
+        public static SeriesIndicatorResult Mfi (this IEnumerable<ICandle> source, int period)
+        {
+            MfiIndicator mfi = new MfiIndicator();
+            MfiOptions options = new MfiOptions { Period = period };
+			
+            return mfi.Get(source.ToArray(), options);
+        }
+    }
+}
---
+++ src/Backend/Core/Trading/TAIndicators/Options/MfiOptions.cs
@@ -0,0 +1,9 @@
+﻿using Contracts.Trading;
+
+namespace core.Trading.TAIndicators.Options
+{
+    public class MfiOptions : IOptionsSet
+    {
+        public int Period { get; set; }
+    }
+}
