1e31278f955892ecbccfd61604cdd47eb85e9cfa
Alexander Yermakov <alexandr.v.ermakov@gmail.com>
2020-02-16 21:24:46 +0200

    CT-V2 Added Indicator AC

---
+++ src/Backend/Core/Trading/TAIndicators/Extensions/AccelerationDecelerationExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Contracts;
+using core.Trading.TAIndicators.Options;
+using core.Trading.TAIndicators.Results;
+
+namespace Core.Trading.TAIndicators.Extensions
+{
+    public static class AccelerationDecelerationExtension
+    {
+        public static SeriesIndicatorResult AccelerationDeceleration (this IEnumerable<ICandle> source)
+        {
+            AccelerationDecelerationIndicator accDec = new AccelerationDecelerationIndicator();
+            return accDec.Get(source.ToArray(), new EmptyOption());
+        }
+    }
+}
