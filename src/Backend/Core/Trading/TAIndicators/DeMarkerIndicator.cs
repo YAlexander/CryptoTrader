46693d854beec938b293c9c729a2f04436a3850c
Alexander Yermakov <alexandr.v.ermakov@gmail.com>
2020-02-17 08:56:44 +0200

    CT-V2 Added DeMarker Indicator

---
+++ src/Backend/Core/Trading/TAIndicators/DeMarkerIndicator.cs
@@ -0,0 +1,53 @@
+﻿using Contracts;
+using Core.Trading.TAIndicators.Extensions;
+using core.Trading.TAIndicators.Options;
+using core.Trading.TAIndicators.Results;
+
+namespace Core.Trading.TAIndicators
+{
+    public class DeMarkerIndicator : BaseIndicator<DeMarkerOptions, SeriesIndicatorResult>
+    {
+        public override string Name { get; } = "DeMarker’s (DeMarker) Indicator";
+        
+        public override SeriesIndicatorResult Get(ICandle[] source, DeMarkerOptions options)
+        {
+            decimal?[] result = new decimal?[source.Length];
+            
+            decimal?[] deMin = new decimal?[source.Length];
+            decimal?[] deMax = new decimal?[source.Length];
+
+            for (int i = 1; i < source.Length; i++)
+            {
+                deMax[i] = source[i].High > source[i - 1].High ? source[i].High - source[i - 1].High : 0;
+                deMin[i] = source[i].Low < source[i - 1].Low ? source[i - 1].Low - source[i].Low : 0;
+            }
+
+            decimal?[] smaMax = deMax.Sma(options.Period).Result;
+            decimal?[] smaMin = deMin.Sma(options.Period).Result;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (smaMax[i].HasValue && smaMin[i].HasValue)
+                {
+                    result[i] = smaMax[i] / (smaMax[i] + smaMin[i]);
+                }
+                else
+                {
+                    result[i] = null;
+                }
+            }
+
+            return new SeriesIndicatorResult() { Result = result };
+        }
+
+        public override SeriesIndicatorResult Get(decimal[] source, DeMarkerOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override SeriesIndicatorResult Get(decimal?[] source, DeMarkerOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Backend/Core/Trading/TAIndicators/Options/DeMarkerOptions.cs
@@ -0,0 +1,9 @@
+﻿using Contracts.Trading;
+
+namespace core.Trading.TAIndicators.Options
+{
+    public class DeMarkerOptions : IOptionsSet
+    {
+        public int Period { get; set; }
+    }
+}
