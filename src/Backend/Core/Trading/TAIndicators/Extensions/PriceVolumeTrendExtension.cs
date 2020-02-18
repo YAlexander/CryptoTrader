5dfb7fd9a3cce0d86c65dfe1155e231f0efd8462
Alexander Yermakov <alexandr.v.ermakov@gmail.com>
2020-02-18 20:56:46 +0200

    CT-V2 Added PriceVolumeTrend Indicator

---
+++ src/Backend/Core/Trading/TAIndicators/Extensions/PriceVolumeTrendExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Contracts;
+using core.Trading.TAIndicators.Options;
+using core.Trading.TAIndicators.Results;
+
+namespace Core.Trading.TAIndicators.Extensions
+{
+    public static class PriceVolumeTrendExtension
+    {
+        public static SeriesIndicatorResult PriceVolumeTrend (this IEnumerable<ICandle> source)
+        {
+            PriceVolumeTrendIndicator pvt = new PriceVolumeTrendIndicator();
+			
+            return pvt.Get(source.ToArray(), new EmptyOption());
+        }
+    }
+}
