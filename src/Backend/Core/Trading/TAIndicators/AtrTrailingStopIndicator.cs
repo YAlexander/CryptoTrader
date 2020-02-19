e4e2ec900ff334414f52f60fefbb5ff01550046d
Alexander Yermakov <alexandr.v.ermakov@gmail.com>
2020-02-19 08:42:11 +0200

    CT-V2 Added simple Trailing Stop

---
+++ src/Backend/Core/Trading/TAIndicators/AtrTrailingStopIndicator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Contracts;
+using Contracts.Enums;
+using Core.Trading.TAIndicators.Extensions;
+using core.Trading.TAIndicators.Options;
+using core.Trading.TAIndicators.Results;
+
+namespace Core.Trading.TAIndicators
+{
+    public class AtrTrailingStopIndicator : BaseIndicator<AtrTrailingStopOptions, SeriesIndicatorResult>
+    {
+        public override string Name { get; } = "ATR Trailing Stop (ATR TS) Indicator";
+        
+        public override SeriesIndicatorResult Get(ICandle[] source, AtrTrailingStopOptions options)
+        {
+            decimal?[] result = new decimal?[source.Length];
+            decimal?[] ema = source.Ema(options.Period, CandleVariables.CLOSE).Result;
+            decimal?[] atrs = source.Atr(options.Period).Result;
+
+            for (int i = options.Period; i < source.Length; i++)
+            {
+                decimal? loss = atrs[i] * options.Multiplier;
+
+                result[i] = source[i].Close > ema[i] ? source[i].Close - loss : source[i].Close + loss;
+            }
+            
+            return new SeriesIndicatorResult() { Result = result };
+        }
+
+        public override SeriesIndicatorResult Get(decimal[] source, AtrTrailingStopOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override SeriesIndicatorResult Get(decimal?[] source, AtrTrailingStopOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Backend/Core/Trading/TAIndicators/Options/AtrTrailingStopOptions.cs
@@ -0,0 +1,12 @@
+﻿using Contracts.Trading;
+
+namespace core.Trading.TAIndicators.Options
+{
+    public class AtrTrailingStopOptions : IOptionsSet
+    
+    {
+        public int Period { get; set; }
+        
+        public decimal Multiplier { get; set; }
+    }
+}
