fb5579959c45609d63dee8e4943af21e60b2f3b8
Alexander Yermakov <alexandr.v.ermakov@gmail.com>
2020-02-22 22:08:21 +0200

    CT-V2 Candles chart wip

--- src/Backoffice/site/src/App.vue
+++ src/Backoffice/site/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <TradingVue msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div id="chart-holder" class="col-lg-6">
+        <TradingVue msg="Welcome to Your Vue.js App"/>
+      </div>
+      <div class="col-lg-6">aaa</div>
+    </div>
   </div>
 </template>
 
 <script>
-import TradingVue from './components/TradingView.vue'
+import TradingVue from "./components/TradingView.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     TradingVue
   }
-}
+};
 </script>
 
 <style>
@@ -21,7 +25,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/Backoffice/site/src/components/TradingView.vue
+++ src/Backoffice/site/src/components/TradingView.vue
@@ -1,47 +1,50 @@
-<template>
-<!-- TradingVueJs 101 (example from 'Getting Started' ) -->
+<style scoped>
+</style>
 
-<trading-vue :data="chart" :width="this.width" :height="this.height"
+<template>
+  <trading-vue
+    :data="chart"
     :overlays="overlays"
+    :width="this.width"
     :color-back="colors.colorBack"
     :color-grid="colors.colorGrid"
-    :color-text="colors.colorText">
-</trading-vue>
+    :color-text="colors.colorText"
+    :titleTxt="titleTxt"
+  ></trading-vue>
 </template>
 
 <script>
-import TradingVue from 'trading-vue-js'
-import Data from '../../data/data.json'
-import Grin from '../indicators/Grin.js'
-
+import TradingVue from "trading-vue-js";
+import Data from "../../data/data.json";
+import Grin from "../indicators/Grin.js";
 
 export default {
-    name: 'app',
-    components: { TradingVue },
-    methods: {
-        onResize(event) {
-            this.width = window.innerWidth
-            this.height = window.innerHeight
-        }
-    },
-    mounted() {
-        window.addEventListener('resize', this.onResize)
-    },
-    beforeDestroy() {
-        window.removeEventListener('resize', this.onResize)
-    },
-    data() {
-        return {
-            chart: Data,
-            width: window.innerWidth,
-            height: window.innerHeight,
-            colors: {
-                colorBack: '#fff',
-                colorGrid: '#eee',
-                colorText: '#333',
-            },
-	overlays: [Grin]
-        }
+  name: "app",
+  components: { TradingVue },
+  methods: {
+    onResize() {
+      var elem = document.getElementById("chart-holder");
+      this.width = elem.offsetWidth;
     }
-}
+  },
+  mounted() {
+    window.addEventListener("resize", this.onResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  data() {
+    return {
+      chart: Data,
+      width: this.width,
+      colors: {
+        colorBack: "#2b2a2a",
+        colorGrid: "#242323",
+        colorText: "#eee"
+      },
+      titleTxt: "",
+      overlays: [Grin]
+    };
+  }
+};
 </script>
