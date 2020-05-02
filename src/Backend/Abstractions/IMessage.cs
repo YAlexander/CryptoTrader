9810189913244e68d55cd8624f4e6da61420ba98
Alexander Yermakov <alexandr.v.ermakov@gmail.com>
2020-05-02 11:27:13 +0300

    CT-V2 Infrastructure templ

---
+++ src/Backend/Abstractions/IMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions
+{
+	public interface IMessage
+	{
+		string Receiver { get; set; }
+		string Sender { get; set; }
+
+		DateTime Created { get; set; }
+
+		DateTime? Expired { get; set; }
+
+		string Message { get; set; }
+	}
+}
---
+++ src/Backend/Abstractions/INotificator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Abstractions
+{
+	public interface INotificator<INotificatorOptions>
+	{
+		Task Notify(IMessage message);
+	}
+}
